c02d9133c0e5b5e78f7accd627418286903f2c15
Arman <2001armanmaulana@gmail.com>
2021-12-14 22:25:19 +0800

    update js url

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -32,6 +32,6 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
     </script>
-<script src="js/jquery-3.6.0.js"></script>
-<script src="js/owl.carousel.js"></script>
-<script src="js/script.js"></script>
+<script src="{{url('js/jquery-3.6.0.js')}}"></script>
+<script src="{{url('js/owl.carousel.js')}}"></script>
+<script src="{{url('js/script.js')}}"></script>
