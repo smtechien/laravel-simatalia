10f29ab02767a9dcd6548e2bb9ac3b2da28202c4
Arman <2001armanmaulana@gmail.com>
2021-12-27 12:46:02 +0800

    pemerintahan desa page

---
+++ resources/views/pemerintahan/desa.blade.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.0/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.0/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="{{url('css/style.css')}}">
+    <link rel="shortcut icon" href="{{url('img/logo_small.png')}}" type="image/x-icon">
+    <title>Pemerintahan Desa</title>
+</head>
+
+<body>
+    <!-- Navbar -->
+    @include('navbar')
+    <!-- akhir Navbar -->
+    <!-- jumbotron -->
+    <section class="container-fluid row p-5 m-0">
+        <h1 class="p-0 fw-bold mb-3">Pemerintahan Desa</h1>
+        <div class="col-12 col-xl-8 p-0">
+            <p class="url"><span class="text-secondary"><a class="text-secondary text-decoration-none" href="/">Si Mata Lia</a> > <a class="text-secondary text-decoration-none" href="/pemerintahan">Pemerintahan</a> > </span><span class="text-danger fw-bold">Pemerintahan Desa</span></p>
+            <hr>
+            <h4 class="fw-bold mt-4 mb-3">Tanah Merah</h4>
+            <table class="table table-striped table-bordered">
+                <tbody>
+                    <tr>
+                        <td>Kepala Desa</td>
+                        <td>Sidik</td>
+                    </tr>
+                    <tr>
+                        <td>Sekertaris Desa</td>
+                        <td>Taufik Walhidayat, S.Kom</td>
+                    </tr>
+                    <tr>
+                        <td>Bendahara</td>
+                        <td>Dedi Sunardi, S.Pd</td>
+                    </tr>
+                    <tr>
+                        <td>Kasi Pemerintahan</td>
+                        <td>Sabri</td>
+                    </tr>
+                    <tr>
+                        <td>Kaur Perencanaan</td>
+                        <td>Rahmat Hidayat</td>
+                    </tr>
+                    <tr>
+                        <td>Kaur Umum</td>
+                        <td>Siti Sadiah, SE</td>
+                    </tr>
+                    <tr>
+                        <td>Kaur Keuangan</td>
+                        <td>Dahlia Lestari, SE</td>
+                    </tr>
+                    <tr>
+                        <td>Kasi Kesejahteraan</td>
+                        <td>Siti Nurbaya</td>
+                    </tr>
+                    <tr>
+                        <td>Kasi Pelayanan</td>
+                        <td>Jumain</td>
+                    </tr>
+                </tbody>
+            </table>
+            <h4 class="fw-bold mt-4 mb-3">Sambungan</h4>
+            <div class="table-responsive">
+
+                <table class="table table-striped table-bordered align-middle">
+                    <tbody>
+                        <tr>
+                            <td>Kepala Desa</td>
+                            <td>Jumadil</td>
+                        </tr>
+                        <tr>
+                            <td>Sekertaris Desa</td>
+                            <td>Radi Putra</td>
+                        </tr>
+                        <tr>
+                            <td>Bendahara</td>
+                            <td>Irwansyah</td>
+                        </tr>
+                        <tr>
+                            <td>Kasi Pemerintahan</td>
+                            <td>Ulfaniasari</td>
+                        </tr>
+                        <tr>
+                            <td>Kaur Perencanaan</td>
+                            <td>Jamiludin,S.Pi</td>
+                        </tr>
+                        <tr>
+                            <td>Kaur Umum</td>
+                            <td>Zuljali</td>
+                        </tr>
+                        <tr>
+                            <td>Kaur Keuangan</td>
+                            <td>Radi Putra</td>
+                        </tr>
+                        <tr>
+                            <td>Kasi Kesejahteraan</td>
+                            <td>Jumaan</td>
+                        </tr>
+                        <tr>
+                            <td>Kasi Pelayanan</td>
+                            <td>Slipinah</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+        @include('side-berita')
+    </section>
+    <!-- akhir jumbotron -->
+
+    <!-- footer -->
+    @include('footer')
+    <!-- akhir footer -->
+</body>
+
+</html>
