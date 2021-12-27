84af0168734efb4a7c0f4cc69f21a621a61f7353
Arman <2001armanmaulana@gmail.com>
2021-12-27 12:46:35 +0800

    side berita

---
+++ resources/views/side-berita.blade.php
@@ -0,0 +1,26 @@
+<div class="col d-flex flex-column side-content">
+    <h4 class="fw-bolder mb-3">Terbaru</h4>
+    <div class="d-flex flex-column flex-md-row flex-xl-column gap-4">
+        <div class="card pad-40">
+            <img src="{{url('img/img-berita-1.png')}}" alt="berita-1" class="img-fluid ">
+            <div class="card-body px-0 pb-0">
+                <h5 class="card-title fw-bold">Judul Berita</h5>
+                <p class="card-title mb-2 text-muted">01 November 2021</p>
+            </div>
+        </div>
+        <div class="card pad-40">
+            <img src="{{url('img/img-berita-1.png')}}" alt="berita-1" class="img-fluid">
+            <div class="card-body px-0 pb-0">
+                <h5 class="card-title fw-bold">Judul Berita</h5>
+                <p class="card-title mb-2 text-muted">01 November 2021</p>
+            </div>
+        </div>
+        <div class="card pad-40">
+            <img src="{{url('img/img-berita-1.png')}}" alt="berita-1" class="img-fluid">
+            <div class="card-body px-0 pb-0">
+                <h5 class="card-title fw-bold">Judul Berita</h5>
+                <p class="card-title mb-2 text-muted">01 November 2021</p>
+            </div>
+        </div>
+    </div>
+</div>
